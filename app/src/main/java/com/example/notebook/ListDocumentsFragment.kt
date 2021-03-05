a7bdf6cf2f0cfeafb23bc330ece08b1e14a30c74
omelchenkoaleks <omelchenkoaleks@gmail.com>
2021-03-05 16:59:10 +0200

    Custom text in SearchView.

--- app/src/main/java/com/example/notebook/ListDocumentsFragment.kt
+++ app/src/main/java/com/example/notebook/ListDocumentsFragment.kt
@@ -1,10 +1,14 @@
 package com.example.notebook
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import com.example.notebook.databinding.FragmentListDocumentsBinding
 import com.example.notebook.utils.APP_ACTIVITY
 
@@ -24,9 +28,20 @@ class ListDocumentsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // SearchView font nunito_bold.
+        val typeface = ResourcesCompat.getFont(requireContext(), R.font.nunito_bold)
+        binding.searchView.setTypeFace(typeface)
+
         binding.fab.setOnClickListener {
             APP_ACTIVITY.navController.navigate(R.id.action_listDocumentsFragment_to_detailDocumentFragment)
         }
+    }
+
+    // SearchView custom font in android.
+    private fun SearchView.setTypeFace(typeface: Typeface?) {
+        val id = context.resources.getIdentifier("android:id/search_src_text", null, null)
+        val searchText = binding.searchView.findViewById(id) as TextView
+        searchText.typeface = typeface
     }
 
 
